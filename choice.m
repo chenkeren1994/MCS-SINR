bd3a93460fea0125846b809989e7cde5edd7d514
seal <chenkerenqiqi@163.com>
2018-04-01 21:36:47 +0800

    0401

--- choice.m
+++ choice.m
@@ -2,13 +2,46 @@ function [Gc,Gp] = choice( C1,P1 )
 % 轮盘赌选择
 % 输入：交叉后的种群，规模2m
 % 输出：选择后的种群规模m
+    [N,S,GSize] = size(C1);
+    M = GSize;
+    m = M/2;
     Gc = zeros(N,S,m);
     Gp = zeros(N,S,m);
     t = zeros(1,M);
     
     for g = 1:M
-        t(g) = getTarget(C1(:,:,g),P1(:,:,g));
+%         t(g) = getTarget(C1(:,:,g),P1(:,:,g));
+        t(g) = randperm(100,1);
     end
     
+    % 将 {[粒子]，适值}放入元胞数组中
+    for i = 1:M
+        GcCeil{i} = {C1(:,:,i),t(i)};
+        GpCeil{i} = {P1(:,:,i),t(i)};
+    end
+    GcCeil = sortCeil(GcCeil);
+    GpCeil = sortCeil(GpCeil);
+    for i = 1:m
+        Gc(:,:,i) = GcCeil{i}{1};
+        Gp(:,:,i) = GpCeil{i}{1};
+    end
 end
+function G2 = sortCeil(G1)
+    % 将元胞数组按照适值大小进行排序，并返回排序后的元胞数组
+    len = size(G1);
+    M = len(2);
+    for i = 1:M
+        maxIndex = i;
+        for j = (i+1):M
+            if G1{j}{2} > G1{maxIndex}{2}
+                maxIndex = j;
+            end
+        end
+        temp = G1{i};
+        G1{i} = G1{maxIndex};
+        G1{maxIndex} = temp;
+    end
+    G2 = G1;
+end
+
 
--- multi_point_crossing.m
+++ multi_point_crossing.m
@@ -1,4 +1,37 @@
-function [C1,C2,C3,C4,P1,P2,P3,P4]= multi_point_crossing(C1,C2,P1,P2)
+function [Gc,Gp] = multi_point_crossing(C,P)
+% 输入：信道种群和功率种群，容量为m
+% 输出：交叉后的信道种群功率种群，容量为2m
+    [N,S,GSize] = size(C); % 返回基站数、信道数和种群容量
+    
+    %初始化种群
+    Gc = zeros(N,S,2*GSize);
+    Gp = zeros(N,S,2*GSize);
+    randNum = 1:GSize;
+    % 对种群中每个个体进行交叉操作
+    while(~isempty(randNum))
+        %在[1：length(randNum)]数组中随机取两个数，并且在取过后将其删除
+        rand = randsample(length(randNum),2);
+        i1 = rand(1);
+        i2 = rand(2);
+        g1 = randNum(i1);
+        g2 = randNum(i2);
+        randNum([i1,i2])=[];
+        C1 = C(:,:,g1);
+        C2 = C(:,:,g2);
+        P1 = P(:,:,g1);
+        P2 = P(:,:,g2);
+        [C1,C2,C3,C4,P1,P2,P3,P4] = multi_point_crossing_vector(C1,C2,P1,P2);
+        Gc(:,:,g1) = C1;
+        Gc(:,:,g2) = C2;
+        Gc(:,:,g1+GSize) = C3;
+        Gc(:,:,g2+GSize) = C4;
+        Gp(:,:,g1) = P1;
+        Gp(:,:,g2) = P2;
+        Gp(:,:,g1+GSize) = P3;
+        Gp(:,:,g2+GSize) = P4;
+    end
+end
+function [C1,C2,C3,C4,P1,P2,P3,P4]= multi_point_crossing_vector(C1,C2,P1,P2)
 
 % 功能：对个体进行交叉操作
 % 输入：个体1的信道分配矩阵和功率分配矩阵，个体2的信道分配矩阵和功率分配矩阵
@@ -20,3 +53,15 @@ function [C1,C2,C3,C4,P1,P2,P3,P4]= multi_point_crossing(C1,C2,P1,P2)
         P4(i,:) = d;
     end
 end
+ function [b1,b2] = swap( a1,a2,k )
+ % 输入：两个向量，交叉点
+ % 输出：交叉后的两个新向量
+     for i = k:length(a1)
+          temp = a1(i);
+         a1(i) = a2(i);
+         a2(i) = temp;
+     end
+     b1 = a1;
+     b2 = a2;
+ end
+ 
